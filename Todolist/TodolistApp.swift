fa120b8961edd74374c2a82d55e6da10e417fa48
Mousa Sweidan <msweidan@hct.ac.ae>
2023-02-24 10:12:41 +0400

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by msweidan on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
